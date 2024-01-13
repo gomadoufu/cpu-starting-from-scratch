732b2d3e1a50ebe27c047c0edfe6924e03fa360f
gomadoufu <49393637+gomadoufu@users.noreply.github.com>
2024-01-13 23:10:17 +0900

    feat: MMIOを実装

--- Z16CPU_tb.v
+++ Z16CPU_tb.v
@@ -2,6 +2,8 @@ module Z16CPU_tb;
 
     reg i_clk = 1'b0;
     reg i_rst = 1'b0;
+    reg i_button = 1'b0;
+    wire [5:0] o_led;
 
     always #1 begin
         i_clk <= ~i_clk;
@@ -13,8 +15,10 @@ module Z16CPU_tb;
     end
 
     Z16CPU CPU(
-               .i_clk  (i_clk  ),
-               .i_rst  (i_rst  )
+               .i_clk     (i_clk  ),
+               .i_rst     (i_rst  ),
+               .i_button  (i_button ),
+               .o_led     (o_led  )
            );
 
     initial begin
@@ -23,6 +27,12 @@ module Z16CPU_tb;
         #2
          // リセットを落とし、実行開始
          i_rst   = 1'b0;
+        #100
+         // 100クロック後にボタンを押す
+         i_button = 1'b1;
+        #20
+         // 20クロック後にボタンを離す
+         i_button = 1'b0;
         #100
          $finish;
     end
